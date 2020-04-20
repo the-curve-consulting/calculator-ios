a160aee68a8a36970a8ed133cd2805e06882f208
James Ridgway <james@usefulinsurance.co.uk>
2020-04-20 16:44:03 +0100

    Initial Commit

---
+++ calculator-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator-ios
+//
+//  Created by James Ridgway on 20/04/2020.
+//  Copyright © 2020 James Ridgway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
